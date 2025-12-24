44c57605c0f7108fcd386d34d2b582efcc73e129
Randy Westmoreland <randy.westmoreland@gmail.com>
2025-12-24 06:13:38 -0600

    Added more waypoint functionality.

--- Assets/Editor/WaypointManagerWindow.cs
+++ Assets/Editor/WaypointManagerWindow.cs
@@ -1,10 +1,42 @@
 using UnityEditor;
+using UnityEngine;
 
 public class WaypointManagerWindow : EditorWindow
 {
+    [SerializeField] private Transform _waypointOrigin;
+
+    public Transform WaypointOrigin { get => _waypointOrigin; set => _waypointOrigin = value; }
+
     [MenuItem("Waypoint/Waypoints Editor Tools")]
     public static void ShowWindow()
     {
         GetWindow<WaypointManagerWindow>("Waypoints Editor Tools");
+    }
+
+    private void OnGUI()
+    {
+        SerializedObject obj = new(this);
+        EditorGUILayout.PropertyField(obj.FindProperty("_waypointOrigin"));
+
+        if (_waypointOrigin == null)
+        {
+            EditorGUILayout.HelpBox("Please assign a Waypoint Origin Transform.", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.BeginVertical("box");
+            CreateButtons();
+            EditorGUILayout.EndVertical();
+        }
+
+        obj.ApplyModifiedProperties();
+    }
+
+    private void CreateButtons()
+    {
+        if (GUILayout.Button("Create Waypoint"))
+        {
+            // Create Waypoint Logic
+        }
     }
 }
